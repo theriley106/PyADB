7315487426149920955a298bbd23f80cec9aa7ae
theriley106 <christopherlambert106@gmail.com>
2016-12-30 19:03:21 -0500

    a

--- Example/Example2.py
+++ Example/Example2.py
@@ -6,8 +6,9 @@ import time
 import csv
 
 
-ButtonsToClick = ["com.checkpoints.app:id/play_all_button", "com.checkpoints.app:id/tab_videos"]
-
+ButtonsToClick = ["com.checkpoints.app:id/play_all_button", "com.checkpoints.app:id/tab_videos", "android:id/button1", 'com.android.launcher:id/launcher', "com.checkpoints.app:id/sexy_dialog_message"]
+def get_num(x):
+    return int(''.join(ele for ele in x if ele.isdigit()))
 def KillADB():
 	Command = 'sudo adb kill-server'
 	os.system(str(Command))
@@ -22,21 +23,23 @@ def LoadDevicesFromCSV(Location):
 	Devices = [l[0] for l in RawDeviceRows]
 	random.shuffle(Devices)
 	IPs = Devices
+	print(str(IPs))
 	Devices = []
 	for IP in IPs:
 		Connected = False
-		if 'unable' not in os.popen('sudo adb connect ' + str(IP)).read():
-			os.system('sudo adb -s ' + str(IP) + ' shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0')
-			os.system('sudo adb -s ' + str(IP)  + ' shell settings put system screen_brightness 0')
+		if 'connected' in os.popen('sudo adb connect ' + str(IP)).read():
+			os.system('sudo adb -s ' + str(IP) + ':5555 shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0')
+			os.system('sudo adb -s ' + str(IP)  + ':5555 shell settings put system screen_brightness 0')
 			Devices.append(str(IP) + ':5555')
 			Connected = True
 			print(str(IP) + ' Connected')
 		if Connected == False:
 			print(str(IP) + ' Not Connected')
+	print(str(Devices))
 	return Devices
 
 def FindButton(button, udid):
-	SavedFile = str(udid[:-5]) + str(random.randint(99,9999999999)) + '.xml'
+	SavedFile = str(udid[:-5]) + '.xml'
 	Command = "sudo adb -s {} pull $(adb -s {} shell uiautomator dump | grep -oP '[^ ]+.xml') {}".format(str(udid), str(udid), SavedFile)
 	os.system(Command)
 	xmldoc = minidom.parse(SavedFile)
@@ -70,12 +73,21 @@ def StartApplication(udid):
 			time.sleep(random.randint(30,120) + 30)
 			while ResetCheck < (100 * len(ButtonsToClick)):
 				for button in ButtonsToClick:
-					os.system(FindButton(button, udid))
+                                        command = FindButton(button, udid)
+                                        os.system(command)
+					if str(button) == "android:id/button1" and 'input tap' in str(command):
+                                            time.sleep(random.randint(12, 45))
+                                            os.system('sudo adb -s ' + udid + " shell monkey -p com.checkpoints.app -c android.intent.category.LAUNCHER 1")
+                                            time.sleep(random.randint(30,80))
+                                        if str(button) == 'com.android.launcher:id/launcher' and 'input tap' in str(command):
+                                            time.sleep(random.randint(12, 45))
+                                            os.system('sudo adb -s ' + udid + " shell monkey -p com.checkpoints.app -c android.intent.category.LAUNCHER 1")
+                                            time.sleep(random.randint(30,80))
 					time.sleep(random.randint(5,15) + 20)
 					ResetCheck = ResetCheck + 1
 			time.sleep(random.randint(120, 480))
-		except:
-			print('error')
+		except BaseException as exp:
+			print('error' + str(exp))
 			time.sleep(random.randint(1200, 2000))
 
 KillADB()
--- Main.py
+++ Main.py
@@ -1,7 +1,23 @@
 import os
 import threading
+import time
+import random
 Devices = []
-
+Cisco1PTV = ["192.168.2.131", "192.168.2.132", "192.168.2.133", "192.168.2.134", "192.168.2.135"]
+RouterPTV = ["192.168.2.91", "192.168.2.92", "192.168.2.93", "192.168.2.94", "192.168.2.95"]
+RouterPPQ = ["192.168.2.96", "192.168.2.97", "192.168.2.98", "192.168.2.99", "192.168.2.90"]
+Cisco1PPQ = ["192.168.2.128", "192.168.2.129", "192.168.2.130", "192.168.2.136", "192.168.2.137"]
+PTV = RouterPTV + Cisco1PTV
+PPQ = RouterPPQ + Cisco1PPQ
+Cisco1 = Cisco1PPQ + Cisco1PTV
+Router = RouterPPQ + RouterPTV
+Devices = Cisco1 + Router
+WirelessDevices = []
+for udid in Devices:
+        wireless = str(udid) + ':5555'
+        WirelessDevices.append(wireless)
+        
+        
 def RestartADB(Devices):
 	Command = 'sudo adb kill-server'
 	os.system(str(Command))
@@ -10,9 +26,18 @@ def RestartADB(Devices):
 def ConnectDevice(udid):
 	Command = 'sudo adb connect ' + udid
 	os.system(str(Command))
+def Rotate(udid):
+        Command = 'sudo adb -s ' + udid + ' shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0'
+        os.system(str(Command))
+def KeycodePower(udid):
+	Command = 'sudo adb -s ' + udid + ":5555 shell input keyevent 26"
+	os.system(str(Command))
 def KeycodeUp(udid):
 	Command = 'sudo adb -s ' + udid + " shell input keyevent 19"
 	os.system(str(Command))
+def SwipeUp(udid):
+        Command = 'sudo adb -s ' + udid + " shell input swipe 0 500 0 0"
+        os.system(str(Command))
 def KeycodeDown(udid):
 	Command = 'sudo adb -s ' + udid + " shell input keyevent 20"
 	os.system(str(Command))
@@ -33,7 +58,9 @@ def KeycodeEnter(udid):
 	os.system(str(Command))
 def TakeScreenshot(udid):
 	screenshot = str(udid) + '.png'
-	Command = "sudo adb -s " + str(udid) + " shell screencap -p | sed 's/\r$//' > " + screenshot
+	print(screenshot)
+	Command = "sudo adb -s " + str(udid) + ":5555 shell screencap -p | sed 's/\r$//' > screen1.png"# + str(screenshot)
+        print(str(Command))
 	os.system(str(Command))
 def StartApplication(udid, app):
 	Command = 'sudo adb -s ' + udid + " shell monkey -p " + str(app) + " -c android.intent.category.LAUNCHER 1"
@@ -50,3 +77,14 @@ def PressButton(udid, image):
 		os.system(str(Command))
 	elif Current is None:
 		print('error')
+RestartADB(Devices)
+for udid in Router:
+        Command = 'sudo adb -s ' + udid + ":5555 shell input text treypass1190"
+        os.system(str(Command))
+        print('done')
+for udid in Router:
+        Command = 'sudo adb -s ' + udid + ":5555 shell input keyevent 66"
+        os.system(str(Command))
+        print('done')
+        time.sleep(random.randint(1,8))
+
