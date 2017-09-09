c167178e5884df74ea56e7ee8f371749ae185b9a
Theriley106 <christopherlambert106@gmail.com>
2017-09-09 14:17:50 -0400

    Fixed Robinhood

--- Robinhood.py
+++ Robinhood.py
@@ -5,7 +5,7 @@ import time
 import RPi.GPIO as GPIO
 import time
 import os
-import pyautogui
+#import pyautogui
 
 GPIO.setmode(GPIO.BCM)
 
@@ -15,20 +15,22 @@ def ForceClose(udid, app):
 	os.system('adb -s {} shell am force-stop {}'.format(udid, app))
 
 while True:
+	time.sleep(.5)
     input_state = GPIO.input(24)
     if input_state == False:
-		a = Android.ConnectedDevices()[0]
-		ForceClose(a, 'com.robinhood.android')
-		time.sleep(1)
-		Android.StartApplication(a, 'com.robinhood.android')
-		time.sleep(2)
-		Android.Scroll(a, 10, 1000, 10, 400)
-		interactions.GrabUiAutomator(a)
-		interactions.click(a, text="TSLA")
-		time.sleep(3)
-		interactions.GrabUiAutomator(a)
-		interactions.click(a, text="BUY")
-		time.sleep(2)
-		interactions.InputText(a, '10000')
-		interactions.GrabUiAutomator(a)
-		interactions.click(a, resource_id="com.robinhood.android:id/review_order_btn")
+    	break
+a = Android.ConnectedDevices()[0]
+ForceClose(a, 'com.robinhood.android')
+time.sleep(1)
+Android.StartApplication(a, 'com.robinhood.android')
+time.sleep(2)
+Android.Scroll(a, 10, 1000, 10, 400)
+interactions.GrabUiAutomator(a)
+interactions.click(a, text="TSLA")
+time.sleep(3)
+interactions.GrabUiAutomator(a)
+interactions.click(a, text="BUY")
+time.sleep(2)
+interactions.InputText(a, '10000')
+interactions.GrabUiAutomator(a)
+interactions.click(a, resource_id="com.robinhood.android:id/review_order_btn")
