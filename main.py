f82221f638001321c42da0f702fb20daadfe8d39
Theriley106 <christopherlambert106@gmail.com>
2017-12-03 17:20:49 -0500

    removed extra files

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-import interactions
-
-class automation(object):
-	def __init__(self, udid, app, ss=False):
-		print('Initiated New Thread With: {}\nAutomation: {}'.format(udid, app))
-		self.udid = udid
-		self.app = app
